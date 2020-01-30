f7193d161fe1e0ec2451150773026f88d2467366
ugljesa.dragisic@gmail.com <ugljesa.dragisic@gmail.com>
2020-01-30 14:10:38 +0100

    Added blogs model.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
-    #'blog.apps.BlogConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
