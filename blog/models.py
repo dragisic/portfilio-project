b5f6a5425c4e453763ffacf5bda4d0a64b9f334e
ugljesa.dragisic@gmail.com <ugljesa.dragisic@gmail.com>
2020-01-31 12:09:25 +0100

    Added blog details

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,15 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
-    pup_date = models.DateField(auto_now_add=True)
-    body = models.CharField(max_length=200)
+    pup_date = models.DateTimeField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pup_date.strftime('%b %e %Y')
